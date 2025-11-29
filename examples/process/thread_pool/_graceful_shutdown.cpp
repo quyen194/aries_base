1a94eafa2c9bb01ddd2415c9ccf69f977b95ae5d
Nguyễn Công Quyền <quyen19492@gmail.com>
2025-11-29 15:51:06 +0700

    Add more Examples & Tests

---
+++ examples/process/thread_pool/_graceful_shutdown.cpp
@@ -0,0 +1,48 @@
+/********************************************************************
+  Copyright 2025, Cong Quyen Knight. All rights reserved
+
+  project:   Aries Base
+  author:    quyen19492
+  email:     quyen19492@gmail.com
+
+  created:   2025/11/28 06:00
+  filename:  aries_base/examples/process/thread_pool/_graceful_shutdown.cpp
+
+  purpose:   Demonstrate graceful shutdown of thread pool
+*********************************************************************/
+
+
+// -----------------------------------------------------------------------------
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+#include "aries_base/process/thread_pool/thread_pool.hpp"
+// -----------------------------------------------------------------------------
+
+
+// -----------------------------------------------------------------------------
+using namespace std::chrono_literals;
+using namespace process;
+// -----------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------
+
+int main() {
+  std::cout << "=== ThreadPool graceful shutdown example ===\n";
+  ThreadPool::CreateInstance(4, 16);
+
+  for (int i = 0; i < 8; ++i) {
+    ThreadPool::PostTask([i]{
+      std::this_thread::sleep_for(50ms);
+      std::cout << "task " << i << " done\n";
+    });
+  }
+
+  std::this_thread::sleep_for(500ms);
+  std::cout << "destroying pool...\n";
+  ThreadPool::DestroyInstance();
+  std::cout << "done.\n";
+  return 0;
+}
+// -----------------------------------------------------------------------------
